9f2f40061920d90b91c6cc86251701523d79f2d1
Flerys11 <118915509+Flerys11@users.noreply.github.com>
2023-03-14 19:59:50 +0300

    Add files via upload
    premier etape sprint

---
+++ WEB-INF/Files/Mapping.java
@@ -0,0 +1,30 @@
+package etu2044.framework;
+
+public class Mapping {
+    String className;
+    String Methode;
+
+    public Mapping(String className, String methode) {
+        setClassName(className);
+        setMethode(methode);
+    }
+
+    public Mapping(){}
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethode() {
+        return Methode;
+    }
+
+    public void setMethode(String methode) {
+        Methode = methode;
+    }
+    
+}
